b22061053d8cbfa63f4d6dd7841acd5fc532f9f3
guganems <g_nemsveridze@cu.edu.ge>
2020-07-23 12:00:00 +0400

    dasd

--- besteverselect.js
+++ besteverselect.js
@@ -4,11 +4,11 @@ class BestEverSelect {
         this.init();
     }
     init() {
+        this.getData();
         let element = document.getElementById(this.id);
-        this.addInput(element);
+        this.drawSelectbox(element);
         element.addEventListener('click', function () {
             document.getElementById('shittySelectInput').style.display = "block";
-
         });
         document.getElementById('shittySelectInput').addEventListener('keyup', function () {
             let searchValue = document.getElementById('shittySelectInput').value;
@@ -29,8 +29,37 @@ class BestEverSelect {
             }
         });
     }
+    getData() {
+        let data = [];
+        let obj = {};
+        let element = document.getElementById(this.id);
+        for (let option of element.options) {
+            obj = {
+                text: option.text,
+                value: option.value
+            };
+            data.push(obj);
+        }
+        this.data = data;
+    }
+    drawSelectbox(element) {
+        let data = "";
+        for (let entry of this.data) {
+            data += `<li data-value="${entry.value}">${entry.text}</li>`;
+        }
+        let input = `
+            <div>
+                <span><input style="display: none;" type="text" id="shittySelectInput"/></span>
+                <span>
+                    <ul>
+                        ${data}
+                    </ul>
+                </span>
+            </div>
+        `;
+    }
     addInput(element) {
-        let input = `<input style="display: none;" type="text" id="shittySelectInput"/>`;
+        let input = `<input style="display: none; border-radius: 10px;" type="text" id="shittySelectInput"/>`;
         element.insertAdjacentHTML('beforebegin', input);
     }
 }
